ae4c9f66669696b67a9a22ddfbd43b78617298c4
GhosTea1570 <nitinpandey1570@gmail.com>
2026-09-07 19:18:30 -0400

    Object Calling
    Creating objects that can be called onto one central object.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotLocationPractice.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode;
+
+public class RobotLocationPractice {
+
+    double angle;
+    double x;
+    double y;
+
+
+
+    public RobotLocationPractice(double angle) { // Constructor Method
+        this.angle = angle;
+    }
+
+    public double getHeading() { // Getter Method
+        // this method normalizes robot heading between -180 and 180
+        // useful for turn angles when crossing 0 - 360 boundary
+        double angle = this.angle;
+        while (angle > 180) {
+            angle -= 360; // subtract until in target range
+        }
+        while (angle <= 180) {
+            angle += 360; // add until in target range
+        }
+        return angle; // return normalized value
+    }
+
+    public void setAngle(double angle) { // Setter Method
+        this.angle = angle;
+    }
+
+    public void turnRobot(double angleChange) {
+        angle -= angleChange;
+    }
+
+    public double getAngle(){
+        return this.angle;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public void setX(double xChange) {
+        this.x = xChange;
+    }
+    public void changeX(double changeAmount) {
+        this.x += changeAmount;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setY(double yChange) {
+        this.y = yChange;
+    }
+    public void changeY(double changeAmount) {
+        this.y += changeAmount;
+    }
+
+
+}
